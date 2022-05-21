9948aa71742bb1673d16314751bf546a592fefd2
Special-Week <87489040+Special-Week@users.noreply.github.com>
2022-05-21 14:15:42 +0800

    Update __init__.py

--- src/plugins/nonebot_plugin_status/__init__.py
+++ src/plugins/nonebot_plugin_status/__init__.py
@@ -1,7 +1,6 @@
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import MessageSegment
 import psutil
-import wmi
 from PIL import Image, ImageFont, ImageDraw
 import os
 from io import BytesIO
@@ -16,7 +15,7 @@ command = on_command(
 async def server_status():
     data = psutil.virtual_memory()
     obj_Disk = psutil.disk_usage('/')
-    the_message = ("硬盘总空间:    "+str(round(obj_Disk.total / (1024.0 ** 3), 2))+"GB")+"\n"+("硬盘已使用:    "+str(round(obj_Disk.used / (1024.0 ** 3), 2))+"GB")+"\n"+("硬盘剩余:      "+str(round(obj_Disk.free / (1024.0 ** 3), 2))+"GB")+"\n"+("硬盘占用率:    "+str(round(obj_Disk.percent, 2))+"%")+"\n"+("CPU型号:       " + wmi.WMI().Win32_Processor()[0].Name) + \
+    the_message = ("硬盘总空间:    "+str(round(obj_Disk.total / (1024.0 ** 3), 2))+"GB")+"\n"+("硬盘已使用:    "+str(round(obj_Disk.used / (1024.0 ** 3), 2))+"GB")+"\n"+("硬盘剩余:      "+str(round(obj_Disk.free / (1024.0 ** 3), 2))+"GB")+"\n"+("硬盘占用率:    "+str(round(obj_Disk.percent, 2))+"%")+ \
         "\n"+("物理核心:      "+str(psutil.cpu_count(logical=False)))+"\n"+('CPU占用率:     ' + str(psutil.cpu_percent(interval=1))+'%')+"\n"+("总内存:        " + str(round(int(data.total) / 1024 / 1024 / 1024, 2))+"GB") + \
         "\n"+("可用内存：     " + str(round(int(data.available) / 1024 / 1024 / 1024, 2)
                             )+"GB")+"\n"+("内存占用率:    %d" % (int(round(data.percent))) + "%")
