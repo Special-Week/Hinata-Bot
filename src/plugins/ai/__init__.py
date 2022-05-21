6aa7404a884ebee3286aeb8249e72245674cce80
Special-Week <87489040+Special-Week@users.noreply.github.com>
2022-05-21 21:18:31 +0800

    Update __init__.py

--- src/plugins/ai/__init__.py
+++ src/plugins/ai/__init__.py
@@ -112,7 +112,7 @@ async def _(bot: Bot, event: MessageEvent):
         nickname = event.sender.nickname
     result = await get_chat_result(msg, event.user_id, nickname)
     if result == None:
-        await ai.finish(Message(str(requests.get(f"http://api.qingyunke.com/api.php?key=free&appid=0&msg={msg}").json()["content"]).replace("林欣","星野日向_Official").replace("{br}","\n").replace("贾彦娟","星野日向_Official").replace("周超辉","星野日向_Official").replace("鑫总","星野日向_Official").replace("张鑫","星野日向_Official").replace("菲菲","Hinata").replace("dn","星野日向_Official").replace("1938877131","2749903559").replace("小燕","Hinata")))
+        await Anime.finish(Message(str(requests.get(f"http://api.qingyunke.com/api.php?key=free&appid=0&msg={msg}").json()["content"]).replace("林欣","星野日向_Official").replace("{br}","\n").replace("贾彦娟","星野日向_Official").replace("周超辉","星野日向_Official").replace("鑫总","星野日向_Official").replace("张鑫","星野日向_Official").replace("菲菲","Hinata").replace("dn","星野日向_Official").replace("1938877131","2749903559").replace("小燕","Hinata")))
     await Anime.finish(Message(result))
 
 
