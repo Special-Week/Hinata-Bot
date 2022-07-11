09caafc1da954dc8ee9bc760be50fc7a7e884400
Special-Week <87489040+Special-Week@users.noreply.github.com>
2022-07-11 18:13:44 +0800

    Update 换源安装一些依赖.bat

--- 换源安装一些依赖.bat
+++ 换源安装一些依赖.bat
@@ -1,4 +1,3 @@
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip install nb-cli nonebot-adapter-onebot pillow cn2an psutil aiocache aiohttp jieba aiofiles imageio fontTools ujson pinyin pyenchant opencv-python emoji pypinyin dateparser lxml cachetools bs4 nonebot_plugin_apscheduler nonebot_plugin_htmlrender wmi
 pause
-
